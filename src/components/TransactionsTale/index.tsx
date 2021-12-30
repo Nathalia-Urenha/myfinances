8e1d6919aad8bdd3e0b3a45cbf3a9b86a86289f5
Nathalia-Urenha <naty.urenha@gmail.com>
2021-12-30 15:57:38 -0300

    Configurando  MirageJs

--- src/components/TransactionsTale/index.tsx
+++ src/components/TransactionsTale/index.tsx
@@ -1,16 +1,23 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionTable(){
+    useEffect(()=>{
+        api.get('/transactions')
+        .then(response => console.log(response.data))
+    }, []);
     return(
         <Container>
             <table>
                 <thead>
+                    <tr>
                     <th>Título</th>
                     <th>Valor</th>
                     <th>Categoria</th>
                     <th>Data</th>
+                    </tr>
                 </thead>
-
                 <tbody>
                     <tr>
                         <td>Desenvolvimento de Website</td>
