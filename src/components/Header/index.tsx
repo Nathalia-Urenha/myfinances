2df4ac3c64b6376c0bb998aa826628c1599608ab
Nathalia-Urenha <naty.urenha@gmail.com>
2021-12-30 13:55:54 -0300

    Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import logoImg from '../../assets/logo.svg'
+import { Container, Content } from './styles'
+
+export function Header(){
+    return(
+        <Container>
+            <Content>
+                <img src={logoImg} alt="My Finances" />
+                <button type="button">Nova Transação</button>
+            </Content>
+        </Container>
+    )
+}
