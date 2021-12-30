1451b4641da3ad3fcb6a198c47d562e515347a15
Nathalia-Urenha <naty.urenha@gmail.com>
2021-12-30 16:40:27 -0300

    Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,18 @@
-import logoImg from '../../assets/logo.svg'
-import { Container, Content } from './styles'
+import { useState } from 'react';
+import logoImg from '../../assets/logo.svg';
+import { Container, Content } from './styles';
 
-export function Header(){
+interface HeaderProps{
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal} : HeaderProps){
+
     return(
         <Container>
             <Content>
                 <img src={logoImg} alt="My Finances" />
-                <button type="button">Nova Transação</button>
+                <button type="button" onClick={onOpenNewTransactionModal}>Nova Transação</button>
             </Content>
         </Container>
     )
