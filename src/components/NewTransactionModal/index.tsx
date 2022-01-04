e87c26cef44da588920b93412bfd8ca482b3b71a
Nathalia-Urenha <naty.urenha@gmail.com>
2022-01-03 21:49:47 -0300

    Inserindo transações na API

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,7 @@ import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import { FormEvent, useState } from 'react';
+import { api } from '../../services/api';
 
 
 interface NewTransactionModalProps{
@@ -21,12 +22,14 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
     function handleCreateNewTransaction(event: FormEvent){
         event.preventDefault();
 
-        console.log({
+       const data ={
             title, 
             value, 
             category, 
             type, 
-        });
+        };
+
+        api.post('/transactions', data)
 
     }
 
